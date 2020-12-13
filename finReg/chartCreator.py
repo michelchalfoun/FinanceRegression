fa113594b738ef240ed40ef3d610b3f975ba3173
michelchalfoun <mikachalfoun@gmail.com>
2020-12-12 22:57:34 -0500

    Created function for plotting

---
+++ finReg/chartCreator.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import json
+import dataHandler as dh
+
+# Retrieves the API key needed to process the http request
+with open("config.json") as json_data_file:
+    data = json.load(json_data_file)
+    apiKey = data['FinancialModeling']['apiKey']
+
+# Plot a one or multiple ratios given an array of ratios
+def plotRatios(ticker, profitabilityRatios):
+    for i in profitabilityRatios:
+        years, ratios = dh.getRatios(ticker, i, apiKey)
+        plt.plot(years, ratios, label=i)
+
+    if (len(profitabilityRatios) == 1):
+        plt.title(profitabilityRatios[0] + " of " + ticker + " per year")
+    else:
+        plt.title("Profitability ratios of " + ticker + " per year")
+        plt.legend(loc='upper right')
+    plt.show()
+
+plotRatios("AAPL", ["grossProfitMargin", "netProfitMargin", "effectiveTaxRate"])
