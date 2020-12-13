88d8dc6660b20101e192ed3faa51a848d01e6e23
michelchalfoun <mikachalfoun@gmail.com>
2020-12-13 13:18:06 -0500

    Cleaned main

--- finReg/main.py
+++ finReg/main.py
@@ -1,9 +1,7 @@
 import chartCreator as cc
 import dataHandler as dh
-from pprint import pprint
 
-# This function plots the data ratios needed for the ticker
-ticker = "AAPL"
-cc.plotRatios(ticker, ["grossProfitMargin", "netProfitMargin", "effectiveTaxRate"])
-years, values = dh.getAllRatios("TSLA", "d64e47f7b3b94ffa5e7ebe1ba396ceb0")
-pprint(values)
+ticker = "AMZN"
+ratios = ["grossProfitMargin", "cashRatio"]
+cc.plotLinSquareRegression(ticker, ratios, True)
+
