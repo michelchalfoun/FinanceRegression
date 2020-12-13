44f4ea5025c5685c72f57aeeed4dc0aa11f92378
michelchalfoun <mikachalfoun@gmail.com>
2020-12-13 13:27:48 -0500

    Added comments

--- finReg/chartCreator.py
+++ finReg/chartCreator.py
@@ -10,10 +10,13 @@ with open("config.json") as json_data_file:
 
 # Plot one or multiple ratios given an array of ratios
 def plotRatios(ticker, profitabilityRatios):
+    
+    # Get ratios and plot them as individual points with labeling
     for i in profitabilityRatios:
         years, ratios = dh.getRatios(ticker, i, apiKey)
         plt.plot(years, ratios, label=i)
 
+    # Chooses title based on number of passed in ratios
     if (len(profitabilityRatios) == 1):
         plt.title(profitabilityRatios[0] + " of " + ticker + " per year")
     else:
@@ -23,15 +26,24 @@ def plotRatios(ticker, profitabilityRatios):
 
 # Plot the regressions of one or multiple ratios given an array of ratios
 def plotLinSquareRegression(ticker, profitabilityRatios, visualizeWithPoints:bool):
+    
+    # Computes Least Squares Regression for the given ratios
     for i in profitabilityRatios:
         years, ratios = dh.getRatios(ticker, i, apiKey)
+        
+        # Find coefficients of the Least Squares Regression
         m, c = np.polyfit(years, ratios, 1)
+        
+        # Compute individual points of the regression for plotting
         xn = np.linspace(years[0], years[len(years) - 1], 200)
         yn = np.polyval([m, c], xn)
         plt.plot(xn, yn, label=(i + " lin. squares regression"))
+        
+        # Shows the individual points before the regression if requested
         if(visualizeWithPoints):
             plt.plot(years, ratios, label=i)
 
+    # Chooses title based on number of passed in ratios
     if (len(profitabilityRatios) == 1):
         plt.title(profitabilityRatios[0] + " least squares fit of " + ticker + " per year")
     else:
--- finReg/dataHandler.py
+++ finReg/dataHandler.py
@@ -4,43 +4,62 @@ import json
 # getRatio returns a specific ratio in a specific year and quarter
 def getRatio(ticker, ratio, year, quarter, apiKey):
 
+    # Perform the request to the API to retreive all ratios for ticker quarterly
     financial_ratios =  requests.get(f"https://financialmodelingprep.com/api/v3/ratios/" + ticker +"?period=quarter&apikey=" + apiKey)
     financial_ratios = financial_ratios.json()
 
     for i in reversed(range(len(financial_ratios))):
+        
+        # Splits date values to simplify comparisons
         splitDate = financial_ratios[i]['date'].split("-")
+        
+        # Checks if data is non existant
         if (str(year) == splitDate[0]) and ((quarter * 3) == int(splitDate[1])):
             return financial_ratios[i][ratio]
     
     return False
 
-# getRatio returns a specific profitability ratio in all the years available
+# getRatios returns a specific profitability ratio in all the years available
 def getRatios(ticker, ratio, apiKey):
 
+    # Perform the request to the API to retreive all ratios for ticker quarterly
     financial_ratios =  requests.get(f"https://financialmodelingprep.com/api/v3/ratios/" + ticker +"?period=quarter&apikey=" + apiKey)
     financial_ratios = financial_ratios.json()
     years = []
     ratios = []
 
     for i in reversed(range(len(financial_ratios))):
+        
+        # Checks if data is non existant
         if (financial_ratios[i][ratio] != '') and (financial_ratios[i][ratio] != None):
+            
+            # Splits date values to simplify comparisons
             splitDate = financial_ratios[i]['date'].split("-")
+            
+            # Compute quarters as 0, 0.25, 0.5, 0.75 of years
             years.append(float(splitDate[0]) + (float(splitDate[1]) / (12)) - 0.25)
             ratios.append(float(financial_ratios[i][ratio]))
     
     return years, ratios
 
-# getRatio returns all profitability ratios in all the years available
+# getAllRatios returns all profitability ratios in all the years available
 def getAllRatios(ticker, apiKey):
 
+    # Perform the request to the API to retreive all ratios for ticker quarterly
     financial_ratios =  requests.get(f"https://financialmodelingprep.com/api/v3/ratios/" + ticker +"?period=quarter&apikey=" + apiKey)
     financial_ratios = financial_ratios.json()
     years = []
     profitRatios = []
 
     for i in reversed(range(len(financial_ratios))):
+        
+        # Splits date values to simplify comparisons
         splitDate = financial_ratios[i]['date'].split("-")
+        
+        # Compute quarters as 0, 0.25, 0.5, 0.75 of years
         years.append(float(splitDate[0]) + (float(splitDate[1]) / (12)) - 0.25)
+        
+        # Removes unnecessary values from dict
         editedRatioDict = financial_ratios[i]
         del editedRatioDict['symbol']
         del editedRatioDict['date']
